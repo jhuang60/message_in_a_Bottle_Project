739c3deabddfe6cddb25a7608d1b0bbcfd8fa5f1
jhuang60 <jhuang60@hawk.iit.edu>
2021-04-30 03:06:10 -0500

    created ajax request for user to post a message

---
+++ api/resources/postMessage.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+    //onclick to get a message
+    $("#submitMessageBtn").on("click",function(e){
+        e.preventDefault();
+        sendMessage();
+    });
+
+});
+
+//function to send a message
+function sendMessage(){
+    //save user input into variables
+    let anonymousName =  $('#anonymousNameInput').val();
+    let message = $('#messageInput').val();
+
+    //ajax post request for to send a message
+    $.ajax("http://localhost:3000/messages", {
+        type : "POST",
+        data : {    anonymousName: anonymousName,
+                    message: message,
+                },
+        success: result => {
+            console.log("success ", result);
+            alert ("success");
+        },
+        error : error => {
+            console.log("error", error);
+        }
+    });
+
+}
